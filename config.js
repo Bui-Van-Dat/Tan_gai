0e237bfcd37faee61dd443905acd926cc538b689
Bui-Van-Dat <89328299+Bui-Van-Dat@users.noreply.github.com>
2022-07-19 22:44:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Chào cậu, lại là Hiệu đây!',
+    introTitle: 'Chào cậu, lại là Đạt đây!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi. 
